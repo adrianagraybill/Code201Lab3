a587711029ff08abe8e3e159b5424a099194487e
adrianagraybill <adriana.graybill@gmail.com>
2019-02-03 21:42:28 -0800

    added to lab 3

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Code201Lab3
 
 This lab is building on lab 2, I will be adding a numbered list, a couple more questions, and a tally total for the user to see results.
+
+Ryan Webking was the driver, we made a lot of changes by adding 6 and 7. We used arrays and a for loop. After working as a team of 3 we learned the power of arrays and functions. We also used 
+
+Ryan drove for me(Adriana)
+Adriana drove for Jonny
+Jonny drove for Ryan
+
+Adriana navigated Ryan
+Ryan navigated Jonny
+Jonny navigated Adriana
+
+Once Ryan and I finished our end I worked with Jon and made more strucutred complicated arrays/loops, and functions. 
+
+Now that everything has been brought back to me, I have updated my code to match the code I wrote when Jon and I were a team.
--- js/app.js
+++ js/app.js
@@ -1,101 +1,100 @@
 'use strict';
 
-var name = prompt('What\'s your name?');
-console.log(name);
+var user = prompt('What\'s your name?');
+console.log (user);
 
-var counter = 0;
-
-var game = prompt(name + ', are you ready to play 5 questions? \'Y\' or \'N\'');
+var game = prompt (user + ', are you ready to play 5 questions? \'Y\' or \'N\'');
 console.log(game);
 game = game.toLowerCase();
 
 if (game === 'y' || game === 'yes') {
-  console.log('Correct answers: ' + counter);
+  alert('Great!');
 } else {
   alert('Too bad!');
 }
 
-var q1 = prompt('Do I like true crime? \'Y\' or \'N\'') .toUpperCase();
-console.log(q1);
+var counter = 0;
 
-if (q1 === 'Y' || q1 === 'YES') {
-  alert('Correct!');
-  counter++;
-  console.log('Correct answers: ' + counter);
-} else {
-  alert('You\'re wrong!');
+var questions = [
+  'Do I like true crime? Y or N',
+  'Are my sneezes obnoxious? Y or N',
+  'Do I hate watching movies and tv? Y or N',
+  'Do I prefer Iron Man to Captain America? Y or N',
+  'Am I a dog person? Y or N',
+  'What number am I thinking of? Clue: Between 1 and 10',
+  'I speak two languages, can you guess one of them?'
+];
+
+var possibleAnswers = [
+  ['yes', 'YES', 'Yes', 'y', 'Y', 'No', 'NO', 'no', 'N', 'n'],
+  ['yes', 'YES', 'Yes', 'y', 'Y', 'No', 'NO', 'no', 'N', 'n'],
+  ['yes', 'YES', 'Yes', 'y', 'Y', 'No', 'NO', 'no', 'N', 'n'],
+  ['yes', 'YES', 'Yes', 'y', 'Y', 'No', 'NO', 'no', 'N', 'n'],
+  ['yes', 'YES', 'Yes', 'y', 'Y', 'No', 'NO', 'no', 'N', 'n'],
+  ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+  ['Spanish', 'spanish', 'SPANISH',
+    'English', 'english', 'ENGLISH',
+    'French', 'french', 'FRENCH',
+    'Italian', 'italian', 'ITALIAN',]
+];
+
+var correctAnswers = [
+  ['yes', 'YES', 'Yes', 'y', 'Y'],
+  ['yes', 'YES', 'Yes', 'y', 'Y'],
+  ['No', 'NO', 'no', 'N', 'n'],
+  ['yes', 'YES', 'Yes', 'y', 'Y'],
+  ['yes', 'YES', 'Yes', 'y', 'Y'],
+  ['5'],
+  ['Spanish', 'spanish', 'SPANISH',
+    'English', 'english', 'ENGLISH']
+];
+
+var attempts = [1, 1, 1, 1, 1, 4, 6];
+
+var responses = [];
+
+for (var turnIndex = 0; turnIndex < questions.length; turnIndex++) {
+  for (var tryIndex = 0; tryIndex < attempts[turnIndex]; tryIndex++) {
+    var response = '';
+    while (!possibleAnswers[turnIndex].includes(response)) {
+      response = prompt(questions[turnIndex]);
+    }
+    responses.push(response);
+    console.log(response);
+
+    // Check to see if the response is correct
+    if (correctAnswers[turnIndex].includes(response)) {
+      alert('Correct!');
+      counter++;
+      break;
+    } else if (response === '1') {
+      alert('Too low, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '2') {
+      alert('Too low, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '3') {
+      alert('Too low, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '4') {
+      alert('Too low, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '6') {
+      alert('Too high, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '7') {
+      alert('Too high, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '8') {
+      alert('Too high, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '9') {
+      alert('Too high, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    } else if (response === '10') {
+      alert('Too high, ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+      // If they're on their last try:
+    } else if (tryIndex === attempts[turnIndex] - 1) {
+      alert('Incorrect.');
+      // If they have more attempts left:
+    } else {
+      alert('Incorrect. You have ' + (attempts[turnIndex] - tryIndex - 1) + ' left.');
+    }
+  }
 }
 
-var q2 = prompt('Are my sneezes obnoxious? \'Y\' or \'N\'') .toUpperCase();
-console.log(q2);
+alert(user + ', you got ' + counter + ' out of ' +
+    questions.length + ' correct.');
 
-if (q2 === 'Y' || q2 === 'YES') {
-  alert('I\'m very sorry, I wish I could make them quiet..');
-  counter++;
-  console.log('Correct answers: ' + counter);
-} else {
-  alert('You angel, stop lying..');
-}
-
-var q3 = prompt('Do I hate watching movies and tv? \'Y\' or \'N\'') .toUpperCase();
-console.log(q3);
-
-if (q3 === 'Y' || q3 === 'YES') {
-  alert('HA! Nope!');
-} else {
-  alert('I could watch movies and tv everyday for the rest of my life.');
-  counter++;
-  console.log('Correct answers: ' + counter);
-}
-
-var q4 = prompt('Do I prefer Iron Man to Captain America? \'Y\' or \'N\'') .toUpperCase();
-console.log(q4);
-
-if (q4 === 'Y' || q4 === 'YES') {
-  alert('Absolutely.');
-  counter++;
-  console.log('Correct answers: ' + counter);
-} else {
-  alert('I\'m offended. Team Stark all day.');
-}
-
-var q5 = prompt('Am I a dog person? \'Y\' or \'N\'') .toUpperCase();
-console.log(q5);
-
-if (q5 === 'Y' || q5 === 'YES') {
-  alert('I LOVE ALL THE DOGGOS!');
-  counter++;
-  console.log('Correct answers: ' + counter);
-} else {
-  alert('You don\'t know me at all..');
-}
-
-var q6 = prompt('What number am I thinking of between 1 and 10?');
-console.log(q6);
-
-if (q6 === '5') {
-  alert('Correct.');
-  counter++;
-  console.log('Correct answers: ' + counter);
-} else if (q6 === '4'|| q6 === '3' || q6 === '2' || q6 === '1') {
-  alert ('Too low.');
-} else if(q6 === '6' || q6 === '7' || q6 === '8' || q6 === '9' || q6 === '10') {
-  alert ('Too high.');
-} else {
-  alert ('Did you even read the question?');
-}
-
-var lang = ['Spanish' , 'English'] .toLowerCase();
-var q7 = prompt('I speak two languages, can you guess one of them?');
-console.log(q7);
-
-if (lang.includes(q7)) {
-  alert('That is one of them.');
-  counter++;
-  console.log('Correct answers: ' + counter);
-} else {
-  alert('No');
-}
-
-
-alert('Score ' + counter + ' out of 7!');
